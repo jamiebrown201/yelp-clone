57064e2d52e9055160e6ba17360c495b9a9b9413
Jamie Brown <jamiebrown201@hotmail.com>
2016-01-18 12:07:45 +0000

    partial restraunt add feature

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurants have been added' do
+    scenario 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link 'Add a restaurant'
+    end
+  end
+  context 'resturants have been added' do
+    scenario 'display restaurants' do
+    visit '/restaurants'
+    expect(page).to have_content('KFC')
+    expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+end
