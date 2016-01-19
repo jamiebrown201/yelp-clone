ac39cb653708a74fd95b3d87e3d8c2fa328f9d8d
Jamie Brown <jamiebrown201@hotmail.com>
2016-01-19 11:42:38 +0000

    add partial facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-root to: "restaurants#index"
-resources :restaurants do
-  resources :reviews
-end
+  root to: "restaurants#index"
+  resources :restaurants do
+    resources :reviews
+  end
 end
