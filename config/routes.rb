322c1ab8bc280b956842bbc7fea0c81e27f9e7dc
allimac <camillacolser@hotmail.com>
2016-01-20 11:04:49 +0000

    users log in succesfully through facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "restaurants#index"
   resources :restaurants do
     resources :reviews
   end
+
 end
